d987954fff1a6a81ab5ccb9992b179825bec8f5e
KazakhBas1k <darhan.balmukhambet@gmail.com>
2023-02-28 04:27:59 +0600

    feat: Morse Decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,38 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let word = [];
+    for (let i = 0; i < expr.length; i+=10) {
+        word.push(expr.slice(i, i + 10));
+    }
+    let code = [];
+    for (let letter of word) {
+        let codder = "";
+        if (letter == "**********") {
+            codder += " "
+        }
+        for (let i = 0; i < letter.length; i+=2) {
+            if (letter.slice(i,i+2) == "10") {
+                codder += ".";
+            } else if (letter.slice(i,i+2) == "11") {
+                codder += "-";
+            } 
+        }
+        code.push(codder)
+    }
+    let result = ""
+    next: for (let letter of code) {
+        for (let morse in MORSE_TABLE) {
+            if (letter == morse) {
+                result += MORSE_TABLE[morse];
+                continue next;
+            } else if (letter == " ") {
+                result += " ";
+                continue next;
+            }
+        }
+    }
+    return result;
 }
 
 module.exports = {
